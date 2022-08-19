8a0ae439745484e3ec318067b83a609b77222fcd
tejassapkalgithub <111589516+tejassapkalgithub@users.noreply.github.com>
2022-08-19 22:35:23 +0530

    Add files via upload

---
+++ EmployeeManagementUsingJWT/src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+public class EmployeeController {
+	
+	
+	/*
+	 * @PostMapping("/authenticate") public String generateToken(@ResponseBody
+	 * AuthRequest authrequest) throws Exception { try {
+	 * authenticationManager.authenticate(new
+	 * UsernamePasswordAuthenticationToken(authrequest.getUsername(),
+	 * authrequest.getPassword()));
+	 * 
+	 * } catch (Exception e) { // TODO: handle exception throw new
+	 * Exception("Invalid username and password "); } return
+	 * jwtUtil.generateToken(authrequest.getUserName()); }
+	 */
+
+
+}
